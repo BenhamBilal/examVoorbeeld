edc5cef7073e41652a8c3a219295bc1f3e4f3c12
benbilal94 <benhambilal94@gmail.com>
2025-10-23 13:32:22 +0200

    examenvoorbeeld

---
+++ index.js
@@ -0,0 +1,77 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { log } from "node:console";
+const userInput = readline.createInterface({ input, output });
+
+const main = async () => {
+  let lengteBoek;
+  const bepaalInventaaris = async () => {
+    do {
+      lengteBoek = parseFloat(
+        await userInput.question("Hoe groot is je ineventaaris?")
+      );
+      if (lengteBoek < 5 || lengteBoek > 20) {
+        console.log("Foutieve invoer probeer opniuew (min 5 max 20)");
+      }
+    } while (lengteBoek < 5 || lengteBoek > 20);
+
+    return lengteBoek;
+  };
+
+  lengteBoek = await bepaalInventaaris();
+
+  const vulBoekInventaaris = async (lengteBoek) => {
+    let boekInventaaris = [];
+    let teller = 0;
+
+    while (teller != lengteBoek) {
+      let boek = await userInput.question(
+        "Welke boek wil je in plaatsen ? (R voor Roman, T voor thriller, W voor Wetenschap )"
+      );
+
+      switch (boek) {
+        case "R":
+          boek = "Roman";
+          boekInventaaris.push(boek);
+          teller++;
+          break;
+        case "T":
+          boek = "Thriller";
+          boekInventaaris.push(boek);
+          teller++;
+          break;
+        case "W":
+          boek = "Wetenschappen";
+          boekInventaaris.push(boek);
+          teller++;
+          break;
+        default:
+          console.log("Foutieve invoer probeer opnieuw");
+      }
+    }
+    return boekInventaaris;
+  };
+
+  let ineventaaris = await vulBoekInventaaris(lengteBoek);
+
+  const berekenTotaalPrijs = (ineventaaris) => {
+    let kostPrijs = 0;
+    for (let i = 0; i < ineventaaris.length; i++) {
+      switch (ineventaaris[i]) {
+        case "Roman":
+          kostPrijs += 12;
+          break;
+        case "Thriller":
+          kostPrijs += 15;
+          break;
+        case "Wetenschap":
+          kostPrijs += 20;
+          break;
+        default:
+      }
+      return kostPrijs;
+    }
+  };
+  berekenTotaalPrijs(ineventaaris);
+}
+main();
